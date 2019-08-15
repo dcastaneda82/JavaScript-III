0e209b84b16a6464f2e198c604cc6cd558b583f2
daniel.castaneda <d.k.castaneda82@gmail.com>
2019-08-15 16:42:47 -0500

    done
    done now on to the next

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,40 @@
-/* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+// /* The for principles of "this";
+// * in your own words. explain the four principle for the "this" keyword below.
+// *
+// * 1. Global Object Binding will bind 'this' to the window/console making it one big object.
+function whatsMyName(name) {
+    console.log(this);
+    return name;
+  }
+  console.log(whatsMyName('dii'));
+  // whatsMyName("dii");
 
-// Principle 1
+// * 2. Implicit Binding is when 'this' is binded to the object and becomes anything before the period. 
+const who = {
+  greeting: 'Hello',
+  sayHello: function(name) {
+    console.log(`${this.greeting} my name is also ${name}! :) `);
+    console.log(this);
+  }
+};
+console.log(who.sayHello('dii'));
 
-// code example for Window Binding
+// * 3. New binding allows us to use 'this in a constructor function help create new objects by binding attributes.
+function Robot(process) {
+  this.processing = 'Autobots ';
+  this.process = process;
+  this.speak = function() {
+    console.log(`${this.processing} roll ou...awe come on ${this.process}`);
+    console.log(this);
+  };
+}
 
-// Principle 2
+const optimus = new Robot('bumblebee');
+const bumblebee = new Robot('optimus');
 
-// code example for Implicit Binding
+optimus.speak();
+bumblebee.speak();
 
-// Principle 3
-
-// code example for New Binding
-
-// Principle 4
-
-// code example for Explicit Binding
+// * 4. Explicit binding allows us override what the constructor is set to.
+optimus.speak.call(bumblebee);
+bumblebee.speak.apply(optimus);
